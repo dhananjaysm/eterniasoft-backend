9761e1092693649ea3895e92c48274312f700241
Pawan608 <agarwalpawan6089@gmail.com>
2024-02-21 14:12:19 +0530

    Add design file

--- src/vectorDatabase/vectordb.module.ts
+++ src/vectorDatabase/vectordb.module.ts
@@ -7,8 +7,6 @@ import { VectorProduct } from "src/vectorProduct/vectorProduct.model";
     MongooseModule.forRootAsync({
       useFactory: () => ({
         uri: "mongodb+srv://agarwalpawan6089:2lv0CKcKbWRpIvrX@cluster0.d9fm4mu.mongodb.net/?retryWrites=true&w=majority",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
       }),
     }),
     MongooseModule.forFeature([
