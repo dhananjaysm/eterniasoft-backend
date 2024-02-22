14e8ebdc6981f396b5fbce0f3fb5c754ba554e23
Pawan608 <agarwalpawan6089@gmail.com>
2024-02-22 11:31:44 +0530

    Add design file

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   // Enable CORS
   app.enableCors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3001",
-      "http://localhost:3000",
-    ], // Specify the client origin or use '*' for allowing any origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
-    credentials: true, // This is important for sending cookies and authorization headers with CORS
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
   });
-  await app.listen(3004);
+  await app.listen(3000);
 }
 bootstrap();
